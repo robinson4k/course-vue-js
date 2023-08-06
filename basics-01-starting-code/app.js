96ded0ce30572c567faf3761232921218bc35ce7
Robinson Silva <robinson.silva@grupohpar.com>
2023-08-05 23:28:51 -0400

    17. Understanding "methods" in Vue Apps

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             courseGoal: 'Learning Vue.js',
             vueLink: 'https://vuejs.org'
         }
-    }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random()
+            if (randomNumber < 0.5) {
+                return 'Learn more'
+                
+            } else {
+                return 'Master Vue'
+            }
+        }
+    },
 })
 
 app.mount('#user-goal')
